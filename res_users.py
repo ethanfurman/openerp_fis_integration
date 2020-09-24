8df2bdb3795e65f6ce2ced29d4ac04f5fab09364
Ethan Furman <ethan@stoneleaf.us>
2020-09-23 19:51:53 -0700

    properly support xml_id in fis.transmitter_code
    - add xml_id as a calculated field in fis.transmitter_code
    - change the OE_KEY for CNVZO1 to xml_id

--- res_users.py
+++ res_users.py
@@ -80,11 +80,11 @@ class res_users(osv.Model):
 class transmitter(osv.Model):
     _name = 'fis.transmitter_code'
 
-    def _calc_name(self, cr, uid, ids, field_name=None, arg=None, context=None):
+    def _calc_name(self, cr, uid, ids, field_names=None, arg=None, context=None):
         if isinstance(ids, (int, long)):
             ids = [ids]
         res = {}
-        if not ids or field_name != 'name':
+        if not ids or 'name' not in field_names and 'xml_id' not in field_names:
             return res
         for rec in self.read(
                 cr, uid, ids,
@@ -92,14 +92,17 @@ class transmitter(osv.Model):
                 context=context,
                 ):
             if rec['ship_to_code'] and rec['transmitter_name']:
-                text = '%s [%s] %s' % (rec['transmitter_no'], rec['ship_to_code'], rec['transmitter_name'])
+                name = '%s [%s] %s' % (rec['transmitter_no'], rec['ship_to_code'], rec['transmitter_name'])
+                xml_id = '%s-%s' % (rec['partner_xml_id'], rec['ship_to_code'])
             elif rec['ship_to_code']:
-                text = '%s [%s]' % (rec['transmitter_no'], rec['ship_to_code'])
+                name = '%s [%s]' % (rec['transmitter_no'], rec['ship_to_code'])
+                xml_id = '%s-%s' % (rec['partner_xml_id'], rec['ship_to_code'])
             elif rec['transmitter_name']:
-                text = '%s -- %s' % (rec['transmitter_no'], rec['transmitter_name'])
+                name = '%s -- %s' % (rec['transmitter_no'], rec['transmitter_name'])
+                xml_id = '%s' % (rec['partner_xml_id'], )
             else:
-                text = rec['transmitter_no']
-            res[rec['id']] = text
+                name = rec['transmitter_no']
+            res[rec['id']] = {'name': name, 'xml_id': xml_id}
         return res
 
     _columns = {
@@ -111,6 +114,17 @@ class transmitter(osv.Model):
             store={
                 'fis.transmitter_code': (self_ids, [], 10),
                 },
+            multi='xml-name',
+            ),
+        'xml_id': fields.function(
+            _calc_name,
+            string='FIS ID',
+            type='char',
+            size=11,
+            store={
+                'fis.transmitter_code': (self_ids, [], 10),
+                },
+            multi='xml-name',
             ),
         'partner_xml_id': fields.char("Customer #", size=6, help="customer number in FIS"),
         'transmitter_name': fields.char("Transmitter Name", size=128),
