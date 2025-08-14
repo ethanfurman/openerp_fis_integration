6f6098339bbe2b38c5d596cdb5bc4fd61ce2b9fa
Ethan Furman <ethan@stoneleaf.us>
2025-08-14 10:39:36 -0700

    stop tracking BBxXlate (moved to fislib)

--- scripts/fis-oe.source/BBxXlate/bbxfile.py
+++
@@ -1,5 +0,0 @@
-import sys
-if sys.version_info[0] == 2:
-    from ._bbxfile_2 import *
-else:
-    from ._bbxfile_3 import *
--- scripts/fis-oe.source/BBxXlate/fxFNs.py
+++
@@ -1,5 +0,0 @@
-import sys
-if sys.version_info[0] == 2:
-    from ._fxFNs_2 import *
-else:
-    from ._fxFNs_3 import *
